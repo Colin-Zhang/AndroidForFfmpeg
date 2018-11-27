39508798e091b489f2930abe24beef630fd5c39d
zhang <zhangjin@sefon.com>
2018-11-27 17:30:51 +0800

    新增视频音源抽取功能

---
+++ app/src/main/java/com/sefon/zhang/androidforffmpeg/view/getvoice/GetVoiceActivity.kt
@@ -0,0 +1,14 @@
+package com.sefon.zhang.androidforffmpeg.view.getvoice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.sefon.zhang.androidforffmpeg.R
+
+class GetVoiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_voice)
+    }
+}
