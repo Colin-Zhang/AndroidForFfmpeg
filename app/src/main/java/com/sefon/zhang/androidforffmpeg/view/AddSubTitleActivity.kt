6f613dcda9f5411862db8e955d9618ca4b5653bb
zhang <zhangjin@sefon.com>
2018-11-29 17:37:58 +0800

    新增按钮样式

---
+++ app/src/main/java/com/sefon/zhang/androidforffmpeg/view/AddSubTitleActivity.kt
@@ -0,0 +1,15 @@
+package com.sefon.zhang.androidforffmpeg.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.sefon.zhang.androidforffmpeg.R
+
+class AddSubTitleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_sub_title)
+    }
+
+}
